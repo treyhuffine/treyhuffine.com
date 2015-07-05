45dc2d5f65f7b7037b7ed8effcfe883f01b0ad6c
Trey Huffine <treyhuffine@gmail.com>
2015-07-05 14:39:22 -0700

    Add buttons

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
   $('#fullpage').fullpage({
-    sectionsColor : ['#198017', '#511469', '#57c2e6', '#c7a324']
+    sectionsColor : ['#9dd4f2', '#511469', '#57c2e6', '#c7a324']
   });
 });
