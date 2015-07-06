867ef5b9efae348c2b4cacdb08e113f95d48ab8f
Trey Huffine <treyhuffine@gmail.com>
2015-07-05 21:05:13 -0700

    Stop fullpage from jumping on mobile

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 $(document).ready(function() {
-  $('#fullpage').fullpage({
-    sectionsColor : ['#9dd4f2', '#fff', '#656565', '#eea146'],
-  });
+  if(screen.width < 480) {
+  // do any 480 width stuff here, or simply do nothing
+    $('#fullpage').fullpage({
+      sectionsColor : ['#9dd4f2', '#fff', '#656565', '#eea146'],
+      autoScrolling: false
+    });
+  } else {
+  // do all your cool stuff here for larger screens
+    $('#fullpage').fullpage({
+      sectionsColor : ['#9dd4f2', '#fff', '#656565', '#eea146']
+    });
+  }
+
 });
