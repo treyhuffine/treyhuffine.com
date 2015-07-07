6bc638c81350505f0fdf8c9e55b0f9556cc1990b
Trey Huffine <treyhuffine@gmail.com>
2015-07-06 18:55:20 -0700

    Update front page image

--- main.js
+++ main.js
@@ -2,13 +2,13 @@ $(document).ready(function() {
   if(screen.width < 480) {
   // do any 480 width stuff here, or simply do nothing
     $('#fullpage').fullpage({
-      sectionsColor : ['#b2e7ac', '#fff', '#656565', '#eea146'],
+      sectionsColor : ['black', '#fff', '#656565', '#eea146'],
       autoScrolling: false
     });
   } else {
   // do all your cool stuff here for larger screens
     $('#fullpage').fullpage({
-      sectionsColor : ['#b2e7ac', '#fff', '#656565', '#eea146']
+      sectionsColor : ['black', '#fff', '#656565', '#eea146']
     });
   }
 
