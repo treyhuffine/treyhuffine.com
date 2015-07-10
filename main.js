ff9f56d06455ab11d94077aa24382efde1875980
Trey Huffine <treyhuffine@gmail.com>
2015-07-10 10:27:41 -0700

    Add contact form

--- main.js
+++ main.js
@@ -27,4 +27,48 @@ $(window).load(function() {
   $(".button-collapse").sideNav({
     menuWidth: 300
   });
+
+  // Email - don't hack my auth keys doe
+  jQuery(function($)
+  {
+      $("#contact_form").submit(function()
+      {
+          var email = $(".email-form").val();
+          var name = $(".name-form").val();
+          var msg = $(".msg-form").val();
+          $.ajax(
+          {
+              type: "POST",
+              url: "https://mandrillapp.com/api/1.0/messages/send.json",
+              data: {
+                  'key': 'gGQb87_8lBB7rED_ks_bJA',
+                  'message': {
+                      'from_email': email,
+                      'from_name': name,
+                      'headers': {
+                          'Reply-To': email
+                      },
+                      'subject': 'Website Contact Form',
+                      'text': msg,
+                      'to': [
+                      {
+                          'email': 'treyhuffine@gmail.com',
+                          'name': 'Trey Huffine',
+                          'type': 'to'
+                      }]
+                  }
+              }
+          })
+          .done(function(response) {
+              alert('Your message has been sent. Thank you!'); // show success message
+              $(".email-form").val('');
+              $(".name-form").val('');
+              $(".msg-form").val('');
+          })
+          .fail(function(response) {
+              alert('Error sending message.');
+          });
+          return false;
+      });
+  });
 });
