fdee2f608914a22b336d601e9e56af5906579f42
Trey Huffine <treyhuffine@gmail.com>
2017-05-23 21:03:50 -0700

    Minimalist design

--- main.js
+++ main.js
@@ -1,71 +1,113 @@
-$(window).load(function() {
-  $("#load-spinner").hide();
-  $("#main-container").fadeIn(1500);
-  if(screen.width < 480) {
-  // do any 480 width stuff here, or simply do nothing
-    $('#fullpage').fullpage({
-      sectionsColor : ['black', '#f0eaea', '#faf6f6', '#eea146'],
-      autoScrolling: false,
-      anchors: ['Welcome', 'Portfolio', 'About', 'Thanks'],
-      scrollOverflow: true
-    });
-  } else {
-  // do all your cool stuff here for larger screens
-    $('#fullpage').fullpage({
-      sectionsColor : ['black', '#f0eaea', '#faf6f6', '#eea146'],
-      menu: false,
-      anchors:['Welcome', 'Portfolio', 'About', 'Thanks'],
-      navigation: true,
-      navigationPosition: 'right',
-      navigationTooltips: ['Welcome', 'Portfolio', 'About', 'Thanks'],
-      slidesNavigation: true,
-      slidesNavPosition: 'bottom',
-      loopBottom: true
-    });
+// target to give background to
+var $div = document.getElementById("gradient");
+
+// rgb vals of the gradients
+var gradients = [
+  { start: [8,117,152], stop: [29, 138, 173] },
+  { start: [9, 91, 175], stop: [0,175,171] },
+  { start: [0,175,171], stop: [0,175,171] },
+  { start: [0,175,171], stop: [8,117,152] }
+];
+// how long for each transition
+var transition_time = 1;
+// how many frames per second
+var fps = 120;
+
+// interal type vars
+var timer; // for the setInterval
+var interval_time = Math.round(5000/fps); // how often to interval
+var currentIndex = 0; // where we are in the gradients array
+var nextIndex = 1; // what index of the gradients array is next
+var steps_count = 0; // steps counter
+var steps_total = Math.round(transition_time*fps); // total amount of steps
+var rgb_steps = {
+  start: [0,0,0],
+  stop: [0,0,0]
+}; // how much to alter each rgb value
+var rgb_values = {
+  start: [0,0,0],
+  stop: [0,0,0]
+}; // the current rgb values, gets altered by rgb steps on each interval
+
+var prefixes = ["-webkit-","-moz-","-o-","-ms-",""]; // for looping through adding styles
+var div_style = $div.style; // short cut to actually adding styles
+var gradients_tested = false;
+var color1, color2;
+
+// sets next current and next index of gradients array
+function set_next(num) {
+  return (num + 1 < gradients.length) ? num + 1 : 0;
+}
+
+// work out how big each rgb step is
+function calc_step_size(a,b) {
+  return (a - b) / steps_total;
+}
+
+// populate the rgb_values and rgb_steps objects
+function calc_steps() {
+  for (var key in rgb_values) {
+    if (rgb_values.hasOwnProperty(key)) {
+      for(var i = 0; i < 3; i++) {
+        rgb_values[key][i] = gradients[currentIndex][key][i];
+        rgb_steps[key][i] = calc_step_size(gradients[nextIndex][key][i],rgb_values[key][i]);
+      }
+    }
   }
-  $(".fancybox").fancybox();
-  $(".button-collapse").sideNav({
-    menuWidth: 300
-  });
-  // Email - don't hack my auth keys doe
-  jQuery(function($)
-  {
-      $("#contact_form").submit(function()
-      {
-          var email = $(".email-form").val();
-          var name = $(".name-form").val();
-          var msg = $(".msg-form").val();
-          $.ajax(
-          {
-              type: "POST",
-              url: "https://mandrillapp.com/api/1.0/messages/send.json",
-              data: {
-                  'key': 'gGQb87_8lBB7rED_ks_bJA',
-                  'message': {
-                      'from_email': email,
-                      'from_name': name,
-                      'headers': {
-                          'Reply-To': email
-                      },
-                      'subject': 'Website Contact Form',
-                      'text': msg,
-                      'to': [
-                      {
-                          'email': 'treyhuffine@gmail.com',
-                          'name': 'Trey Huffine',
-                          'type': 'to'
-                      }]
-                  }
-              }
-          })
-          .done(function(response) {
-              alert('Message Sent!');
-              $("#contact_form")[0].reset();
-          })
-          .fail(function(response) {
-              alert('Error sending message.');
-          });
-          return false;
-      });
-  });
-});
+}
+
+// update current rgb vals, update DOM element with new CSS background
+function updateGradient(){
+  // update the current rgb vals
+  for (var key in rgb_values) {
+    if (rgb_values.hasOwnProperty(key)) {
+      for(var i = 0; i < 3; i++) {
+        rgb_values[key][i] += rgb_steps[key][i];
+      }
+    }
+  }
+
+  // generate CSS rgb values
+  var t_color1 = "rgb("+(rgb_values.start[0] | 0)+","+(rgb_values.start[1] | 0)+","+(rgb_values.start[2] | 0)+")";
+  var t_color2 = "rgb("+(rgb_values.stop[0] | 0)+","+(rgb_values.stop[1] | 0)+","+(rgb_values.stop[2] | 0)+")";
+
+  // has anything changed on this interation
+  if (t_color1 != color1 || t_color2 != color2) {
+
+    // update cols strings
+    color1 = t_color1;
+    color2 = t_color2;
+
+    // update DOM element style attribute
+    div_style.backgroundImage = "-webkit-gradient(linear, left top, right bottom, from("+color1+"), to("+color2+"))";
+    for (var i = 0; i < 4; i++) {
+      div_style.backgroundImage = prefixes[i]+"linear-gradient(-45deg, "+color1+", "+color2+")";
+    }
+  }
+
+  // test if the browser can do CSS gradients
+  if (div_style.backgroundImage.indexOf("gradient") == -1 && !gradients_tested) {
+    // if not, kill the timer
+    clearTimeout(timer);
+  }
+  gradients_tested = true;
+
+  // we did another step
+  steps_count++;
+  // did we do too many steps?
+  if (steps_count > steps_total) {
+    // reset steps count
+    steps_count = 0;
+    // set new indexs
+    currentIndex = set_next(currentIndex);
+    nextIndex = set_next(nextIndex);
+    // calc steps
+    calc_steps();
+  }
+}
+
+// initial step calc
+calc_steps();
+
+// go go go!
+timer = setInterval(updateGradient,interval_time);
