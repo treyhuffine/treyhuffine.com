257fa339692239b92fe8d48dd7de924eecd49161
Trey Huffine <treyhuffine@gmail.com>
2019-11-02 18:04:45 -0500

    Google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,6 +29,13 @@ module.exports = {
         icon: `src/images/th-icon-black.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-64856743-1',
+        head: false,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
