bcfc0ff9e6238cbfd2793c7d8d7377ed92f8ef4f
Trey Huffine <treyhuffine@gmail.com>
2020-02-12 15:38:57 -0600

    Typo

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -13,7 +13,7 @@ const items = [
   { name: 'Me', path: '/' },
   { name: 'Projects', path: '/projects' },
   { name: 'Work', path: '/work' },
-  { name: 'Eduction', path: '/education' },
+  { name: 'Education', path: '/education' },
 ]
 
 const Sidebar = () => {
